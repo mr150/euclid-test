680a186c7b9cc611966932c3cd563921c555652f
mr150 <im150@bk.ru>
2021-12-20 04:37:36 +0300

    feat: add menu on small screen

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,17 @@ const swiper = new Swiper(sliderElm.parentElement, {
 		clickable: true,
 	},
 });
+
+document.querySelector('.header').classList.add('header--js');
+
+const navMenuElm = document.querySelector('.nav-menu');
+navMenuElm.classList.add('nav-menu--js');
+navMenuElm.classList.remove('nav-menu--open');
+
+const navBtnElm = document.querySelector('.header__btn-nav');
+navBtnElm.classList.add('header__btn-nav--js');
+
+navBtnElm.addEventListener('click', e => {
+	e.target.classList.toggle('btn--burger-open');
+	navMenuElm.classList.toggle('nav-menu--open');
+});
